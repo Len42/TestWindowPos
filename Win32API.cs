704b45cd04c05b7ec389342e694573a637e53495
Len Popp <lpopp@iwsinc.com>
2018-10-19 11:39:25 -0400

    WINDOWPLACEMENT support - wip

---
+++ Win32API.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace org.lmp.TestWindowPos.Win32
+{
+	[Serializable]
+	[StructLayout(LayoutKind.Sequential)]
+	public struct RECT
+	{
+		public int Left;
+		public int Top;
+		public int Right;
+		public int Bottom;
+
+		public RECT(int left, int top, int right, int bottom)
+		{
+			this.Left = left;
+			this.Top = top;
+			this.Right = right;
+			this.Bottom = bottom;
+		}
+	}
+
+	[Serializable]
+	[StructLayout(LayoutKind.Sequential)]
+	public struct POINT
+	{
+		public int X;
+		public int Y;
+
+		public POINT(int x, int y)
+		{
+			this.X = x;
+			this.Y = y;
+		}
+	}
+
+	[Serializable]
+	[StructLayout(LayoutKind.Sequential)]
+	public struct WINDOWPLACEMENT
+	{
+		public int length;
+		public int flags;
+		public int showCmd;
+		public POINT minPosition;
+		public POINT maxPosition;
+		public RECT normalPosition;
+	}
+
+	public static class Funcs
+	{
+		[DllImport("user32.dll")]
+		private static extern bool SetWindowPlacement(IntPtr hWnd, [In] ref WINDOWPLACEMENT lpwndpl);
+
+		[DllImport("user32.dll")]
+		private static extern bool GetWindowPlacement(IntPtr hWnd, out WINDOWPLACEMENT lpwndpl);
+	}
+}
---
+++ WindowPlacement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace org.lmp.TestWindowPos
+{
+	public static class WindowPlacement
+	{
+		private static void Save()
+		{
+			// TODO
+		}
+
+		private static void Load()
+		{
+			// TODO
+		}
+	}
+}
