92a3a07b0b5e396da56915a46c116e489e8180d4
Len Popp <lpopp@iwsinc.com>
2018-10-20 18:19:01 -0400

    Add Description to AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TestWindowPos")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Demo app for Get/SetWindowPlacement")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TestWindowPos")]
